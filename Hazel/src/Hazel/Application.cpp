e5db0722b799c7e482853da53eb861e578bedaae
rodriguezlaraj <jrodriguez.lara@gmail.com>
2022-10-17 18:27:17 +1100

    Shader raw code

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -51,6 +51,70 @@ namespace Hazel {
 
 		unsigned int indices[3] = { 0, 1, 2 };
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		std::string vertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+			out vec3 v_Position;
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = vec4(a_Position, 1.0);	
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+			in vec3 v_Position;
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+			}
+		)";
+
+		/*
+		* #SHADER EXAMPLE!!!
+		#shader vertex
+		#version 330 core
+
+			//Input data into the vertex shader by using in
+			layout(location = 0) in vec4 position; //The location 0 must match the index 0 above. This is a cast to vec4 even though we have vec 2
+		layout(location = 1) in vec2 texCoord;
+
+		//Output data from the vertex shader into the fragment shader
+		out vec2 v_TexCoord;
+
+		uniform mat4 u_MVP; //Model View Projection matrix
+
+		void main()
+		{
+			gl_Position = u_MVP * position; //Send the position to the GPU (gl_Position)
+			v_TexCoord = texCoord;
+		};
+
+		#shader fragment
+			#version 330 core
+
+			//Output data from the fragment shader by using out
+			layout(location = 0) out vec4 color; //The location 0 must match the index 0 above. This is a cast to vec4 even though we have vec 2
+		in vec2 v_TexCoord;
+
+		//uniform vec4 u_Color;
+		uniform sampler2D u_Texture;
+
+		void main()
+		{
+			vec4 texColor = texture(u_Texture, v_TexCoord);
+			color = texColor; //vec4(0.0, 1.0, 0.0, 1.0); //0 is white, 1 is black   [R, G, B, A] //Sends the color for each pixel to the GPU 
+			//color = u_Color; //vec4(0.0, 1.0, 0.0, 1.0); //0 is white, 1 is black   [R, G, B, A]
+		};*/
+
+
+		m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
+
 	}
 
 	Application::~Application()
@@ -94,6 +158,7 @@ namespace Hazel {
 			glClearColor(red, 0.1f, 0.1f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 
+			m_Shader->Bind();
 			glBindVertexArray(m_VertexArray);
 			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -11,6 +11,8 @@
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
+#include "Hazel/Renderer/Shader.h"
+
 namespace Hazel {
 	class HAZEL_API Application
 	{
@@ -39,6 +41,7 @@ namespace Hazel {
 		LayerStack m_LayerStack;
 
 		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
 	private:
 		//There is only going to be one application and wherever we want to access the applicatio because it has important information
 		static Application* s_Instance;
