2005b57d0fac9234f0a44fc8916449da2b9197b6
rodriguezlaraj <jrodriguez.lara@gmail.com>
2022-10-17 17:41:25 +1100

    Triangle

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -37,6 +37,8 @@ namespace Hazel {
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
+
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
 	private:
 		//There is only going to be one application and wherever we want to access the applicatio because it has important information
 		static Application* s_Instance;
